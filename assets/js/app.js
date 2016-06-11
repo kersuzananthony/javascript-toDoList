2087c35b2f9471310b2e9f18dc4d2c0afb306a6c
Kersuzan Anthony <kersuzan.anthony@gmail.com>
2016-06-11 13:31:11 +0800

    Refactor JS

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,29 +1,69 @@
+var app = app || {};
+
 $(document).ready(function() {
 
-    var ToDoItem = function ToDoItem(element) {
+    app.toDoItem = (function(element) {
         var self = this;
-        this.jQueryElement = $(element);
-        this.deleteButton = this.jQueryElement.find('.delete-button');
-        this.isFinished = false;
+        this.toDoElement = $(element);
+        this.isCompleted = false;
         this.isDeleted = false;
+    });
 
-        this.jQueryElement.on('click', function() {
-            self.isFinished = !self.isFinished;
-            $(this).toggleClass('finished');
-        });
-
-        this.deleteButton.on('click', function($event) {
-            $event.stopPropagation();
-            self.isDeleted = !self.isDeleted;
-            $(this).parent().fadeOut(500, function() {
-                $(this).remove();
-            });
+    app.toDoItem.prototype.delete = function(elem) {
+        this.isDeleted = !this.isDeleted;
+        elem.fadeOut(function () {
+            $(this).remove();
         });
     };
 
-    var todos = $('.todo');
-    for (var i = 0; i < todos.length; i++) {
-        var todo = new ToDoItem(todos[i]);
-    }
+    app.toDoItem.prototype.complete = function(elem) {
+        this.isCompleted = !this.isCompleted;
+        elem.toggleClass('finished');
+    };
+
+    app.toDoList = (function() {
+        var self = this;
+        this.toDoListWrapper = $('ul.todos');
+        this.todosElement = [];
+        this.todosObject = [];
+
+        this.addToDoItem = function(text) {
+            console.log("add");
+            var newElement = "<li class='todo'><span class='delete-button'>X</span>" + text + "</li>";
+            self.toDoListWrapper.append(newElement);
+            self.todosElement.push(newElement);
+            self.todosObject.push(new app.toDoItem(newElement));
+            console.log(self.todosObject);
+        };
+
+        var currentTodos = this.toDoListWrapper.find('.todo');
+        for (var i = 0; i < currentTodos.length; i++) {
+            this.todosElement.push(currentTodos[i]);
+            this.todosObject.push(new app.toDoItem(currentTodos[i]));
+        }
+
+        $('.add-todo-input').on('keypress', function(event) {
+            if (event.which === 13) {
+                self.addToDoItem($(this).val());
+                $(this).val("");
+            }
+        });
+
+        this.toDoListWrapper.delegate("li.todo", "click", function() {
+            var index = $('li.todo').index($(this));
+            console.log(self.todosObject[index]);
+            self.todosObject[index].complete($(this));
+        });
+
+        this.toDoListWrapper.delegate("li.todo span", "click", function($event) {
+            $event.stopPropagation();
+            var parent = $($event.target).parent();
+            var index = $('li.todo').index(parent[0]);
+
+            self.todosObject[index].delete(parent);
+            self.todosElement.splice(index, 1);
+            self.todosObject.splice(index, 1);
+        });
+    })();
 
 });
